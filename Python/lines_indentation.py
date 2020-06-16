707de76710719ba06216cdb8643eaa7e3a6da310
Meghrig Pampalian <meghr.mk@gmail.com>
2020-06-16 14:25:20 -0700

    Added lines_indentation.py

---
+++ Python/lines_indentation.py
@@ -0,0 +1,35 @@
+# Please create a TODO List for me
+"""
+1. Study python identifiers
+    a. if statements
+    b. for loops
+    c. while loops
+2. Study class declaration on python
+    a. Encapsulation
+    b. Polymorphism
+    c. Inheritance
+    d. Another topic
+"""
+value = 10
+if value == 10:
+    print("value is 10")
+    print("Another thing")
+else:
+    print("value is NOT 10")
+    print("Is this going to print?")
+
+print("---------------------------")
+item_one = 10
+item_two = 20
+item_three = 30
+total = item_one + \
+        item_two + \
+        item_three
+print(total)
+
+word = 'word'
+sentence = "This is a sentence."
+paragraph = """This is a paragraph. It is\n\n
+made up of multiple lines and sentences."""
+
+print(paragraph)
